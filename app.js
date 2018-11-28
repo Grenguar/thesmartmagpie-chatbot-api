d31073441e3d66ba357178c8bbe9f0328e015855
Igor Soroka <stupbird@gmail.com>
2018-11-28 20:59:19 +0200

    added translation of sample message

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
 const ApiBuilder = require('claudia-api-builder'),
-  api = new ApiBuilder();
+	api = new ApiBuilder(),
+	request = require('request-promise')
+	AWS = require('aws-sdk'),
+	translate = new AWS.Translate();
 
 module.exports = api;
 
-
 api.post('/hello', function () {
-  return {
-		"replies": [
-			{
-				"type": "text",
-				"content": "Test success!"
-			}
-		]
-	};
+	let testString = 'Jag älskar banana';
+	return getTranslatedMessage(testString);
 },{
   success: { contentType: 'application/json' },
   error: { code: 500 }
 });
 
+function getTranslatedMessage(message) {
+	let result = new Promise(function(resolve, reject){
+		let params = {
+			'Text': message, /* required */
+			'SourceLanguageCode': 'auto', /* required */
+			'TargetLanguageCode': 'en', /* required */
+		};
+		translate.translateText(params, function(err, data) {
+			if (err) console.log(err, err.stack);
+			else     
+				resolve(replyObject(data.TranslatedText));
+		});
+	});
+	return result;
+}
+
+function replyObject(message) {
+	return {
+		"replies": [
+			{
+				"type": "text",
+				"content": message
+			}
+		]
+	}
+}
 
 
