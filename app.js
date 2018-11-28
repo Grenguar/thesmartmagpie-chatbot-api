2f18e992fffdd607d6177b2831b2730dd68cea1c
Igor Soroka <stupbird@gmail.com>
2018-11-28 22:41:40 +0200

    added translation in one direction

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const ApiBuilder = require('claudia-api-builder'),
 	api = new ApiBuilder(),
-	request = require('request-promise')
 	AWS = require('aws-sdk'),
-	translate = new AWS.Translate();
+	translate = new AWS.Translate(),
+	BOT_TOKEN = process.env.BOT_TOKEN,
+	recastai = require('recastai').default,
+	request = new recastai.request(BOT_TOKEN, 'en');
 
 module.exports = api;
 
-api.post('/hello', function () {
+api.post('/translate', (req, res) => {
+	const originalMessage = req.body.nlp.source;
+	const conversationId = req.body.conversation.id;
+
 	let testString = 'Jag älskar banana';
-	return getTranslatedMessage(testString);
+	return getTranslatedMessage(originalMessage)
+		.then(result => replyObject(result));
 },{
   success: { contentType: 'application/json' },
   error: { code: 500 }
@@ -17,17 +23,27 @@ api.post('/hello', function () {
 function getTranslatedMessage(message) {
 	let result = new Promise(function(resolve, reject){
 		let params = {
-			'Text': message, /* required */
-			'SourceLanguageCode': 'auto', /* required */
-			'TargetLanguageCode': 'en', /* required */
+			'Text': message,
+			'SourceLanguageCode': 'auto', 
+			'TargetLanguageCode': 'en',
 		};
 		translate.translateText(params, function(err, data) {
-			if (err) console.log(err, err.stack);
-			else     
-				resolve(replyObject(data.TranslatedText));
+			if (err) {
+				console.log(err, err.stack);
+				reject(err);
+			}
+			else {
+				console.log(data);
+				resolve(data.TranslatedText);
+			}
+				
 		});
 	});
 	return result;
+}
+
+function getMessage(message) {
+	return message;
 }
 
 function replyObject(message) {
@@ -40,5 +56,3 @@ function replyObject(message) {
 		]
 	}
 }
-
-
