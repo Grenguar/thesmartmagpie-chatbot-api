afec379cbc95d4f13ff3a12d28c31b20df6d09e6
Igor Soroka <stupbird@gmail.com>
2018-12-08 21:49:53 +0200

    datamap changed a bit

--- datamap.js
+++ datamap.js
@@ -2,7 +2,7 @@ module.exports = {
     "goodbye" : "Goodbye, my friend!",
     "greetings" : "Hello!",
     "start" : "How can I help you?",
-    "location" : "https://goo.gl/maps/yJihJoWTwF62",
+    "location" : "Messuaukio 1, 00520 Helsinki. Here is the link: https://goo.gl/maps/yJihJoWTwF62",
     "food" : "Dinner and coffee breaks will be served",
     "eventtime" : "Event will be started at 18. The end is scheduled at 24",
     "parking" : "There are two designated parking slots for 100 cars",
