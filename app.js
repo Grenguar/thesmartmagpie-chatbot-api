956fe20f78c864353344ba6e431446036d8527ae
Igor Soroka <stupbird@gmail.com>
2019-02-06 18:05:02 +0200

    added custom policy, new claudia deployment file and message structures

--- app.js
+++ app.js
@@ -1,106 +1,120 @@
-const ApiBuilder = require('claudia-api-builder'),
-	api = new ApiBuilder(),
-	AWS = require('aws-sdk'),
-	translate = new AWS.Translate(),
-	BOT_TOKEN = process.env.BOT_TOKEN,
-	recastai = require('recastai').default,
-	request = new recastai.request(BOT_TOKEN, 'en'),
-	docClient = new AWS.DynamoDB.DocumentClient();
+const ApiBuilder = require("claudia-api-builder"),
+  api = new ApiBuilder(),
+  AWS = require("aws-sdk"),
+  translate = new AWS.Translate(),
+  recastai = require("recastai").default,
+  request = new recastai.request(process.env.BOT_TOKEN, "en"),
+  docClient = new AWS.DynamoDB.DocumentClient(),
+  messageStructures = require("./messageStructures.js");
 
 module.exports = api;
 
-api.post('/translate', async (req, res) => {
-	'use strict';
-	const originalMessage = req.body.nlp.source;
-	const translatedMessageObj = await getTranslatedMessage(originalMessage, 'auto', 'en');
-	const originalMsgLanguage = translatedMessageObj.SourceLanguageCode;
-	const analysedText = await request.analyseText(translatedMessageObj.TranslatedText);
-	const intentFromtranslatedMessage = await getIntentFromEngMessage(analysedText);
-	const responseFromDb = await getTranslatedAnswerToUser(intentFromtranslatedMessage, originalMsgLanguage);
-	if (supportedLanguageByDb(originalMsgLanguage)) {
-		return replyObject(responseFromDb);
-	} else {
-		const translatedBotAnswer = await getTranslatedMessage(responseFromDb, 'en', originalMsgLanguage);
-		return replyObject(translatedBotAnswer.TranslatedText);
-	}
-},{
-  success: { contentType: 'application/json' },
-  error: { code: 500 }
-});
+api.post(
+  "/translate",
+  async (req, res) => {
+    "use strict";
+    const originalMessage = req.body.nlp.source;
+    const translatedMessageObj = await getTranslatedMessage(
+      originalMessage,
+      "auto",
+      "en"
+    );
+    const originalMsgLanguage = translatedMessageObj.SourceLanguageCode;
+    const analysedText = await request.analyseText(
+      translatedMessageObj.TranslatedText
+    );
+    const intentFromtranslatedMessage = await getIntentFromEngMessage(
+      analysedText
+    );
+    const responseFromDb = await getTranslatedAnswerToUser(
+      intentFromtranslatedMessage,
+      originalMsgLanguage
+    );
+    if (supportedLanguageByDb(originalMsgLanguage)) {
+      return messageStructures.textMessageReply(responseFromDb);
+    } else {
+      const translatedBotAnswer = await getTranslatedMessage(
+        responseFromDb,
+        "en",
+        originalMsgLanguage
+      );
+      return messageStructures.textMessageReply(
+        translatedBotAnswer.TranslatedText
+      );
+    }
+  },
+  {
+    success: { contentType: "application/json" },
+    error: { code: 500 }
+  }
+);
 
 function getTranslatedMessage(message, sourceLang, targetLang) {
-	return result = new Promise(function(resolve, reject) {
-		let params = {
-			Text : message,
-			SourceLanguageCode : sourceLang, 
-			TargetLanguageCode : targetLang
-		};
-		translate.translateText(params, function(err, data) {
-			if (err) {
-				console.log(err, err.stack);
-				reject(err);
-			} else {
-				console.log("Translated data from: " + JSON.stringify(data));
-				resolve(data);
-			}
-		});
-	});
+  return (result = new Promise(function(resolve, reject) {
+    let params = {
+      Text: message,
+      SourceLanguageCode: sourceLang,
+      TargetLanguageCode: targetLang
+    };
+    translate.translateText(params, function(err, data) {
+      if (err) {
+        console.log(err, err.stack);
+        reject(err);
+      } else {
+        console.log("Translated data from: " + JSON.stringify(data));
+        resolve(data);
+      }
+    });
+  }));
 }
 
 function getTranslatedAnswerToUser(intent, language) {
-	const fallbackLanguage = 'en';
-	const supportedLanguage = supportedLanguageByDb(language);
-	const languageToQueryFromDb = supportedLanguage ? language : fallbackLanguage;
-	const projectExpressionFormation = "#int, " + languageToQueryFromDb;
-	const params = {
-		TableName : "event-routing",
-		KeyConditionExpression : "#int = :intent",
-		ProjectionExpression : projectExpressionFormation,
-		ExpressionAttributeNames: {
-			"#int" : "intent"
-		},
-		ExpressionAttributeValues: {
-			":intent" : intent
-		}
-	};
-	let dbResponse = new Promise(function(resolve, reject) {
-		docClient.query(params, function(err, data) {
-			if (err) {
-				console.log("Unable to query. Error:", JSON.stringify(err, null, 2));
-				reject(err);
-			} else {
-				let responseFromDb = data.Items[0];
-				console.log("Query succeeded. Data: " + JSON.stringify(responseFromDb));
-				resolve(responseFromDb[languageToQueryFromDb]);
-			}
-		});
-	});
-	return dbResponse;
+  const fallbackLanguage = "en";
+  const supportedLanguage = supportedLanguageByDb(language);
+  const languageToQueryFromDb = supportedLanguage ? language : fallbackLanguage;
+  const projectExpressionFormation = "#int, " + languageToQueryFromDb;
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    KeyConditionExpression: "#int = :intent",
+    ProjectionExpression: projectExpressionFormation,
+    ExpressionAttributeNames: {
+      "#int": "intent"
+    },
+    ExpressionAttributeValues: {
+      ":intent": intent
+    }
+  };
+  let dbResponse = new Promise(function(resolve, reject) {
+    docClient.query(params, function(err, data) {
+      if (err) {
+        console.log("Unable to query. Error:", JSON.stringify(err, null, 2));
+        reject(err);
+      } else {
+        let responseFromDb = data.Items[0];
+        console.log("Query succeeded. Data: " + JSON.stringify(responseFromDb));
+        resolve(responseFromDb[languageToQueryFromDb]);
+      }
+    });
+  });
+  return dbResponse;
 }
 
 function supportedLanguageByDb(language) {
-	return language === "en" || language === "sv" || language === "ru" || language === "fi";
+  return (
+    language === "en" ||
+    language === "sv" ||
+    language === "ru" ||
+    language === "fi"
+  );
 }
 
 function getIntentFromEngMessage(message) {
-	const response = message.raw;
-	let intentsArray = response.intents;
-	if (intentsArray.length == 0) {
-		return "agent";
-	} else {
-		intentsArray.sort((i1, i2) => i2.confidence - i1.confidence);
-		return intentsArray[0].slug;
-	}
+  const response = message.raw;
+  let intentsArray = response.intents;
+  if (intentsArray.length == 0) {
+    return "agent";
+  } else {
+    intentsArray.sort((i1, i2) => i2.confidence - i1.confidence);
+    return intentsArray[0].slug;
+  }
 }
-
-function replyObject(message) {
-	return {
-		replies: [
-			{
-				type : "text",
-				content : message
-			}
-		]
-	}
-}
-
---
+++ messageStructures.js
@@ -0,0 +1,93 @@
+module.exports = {
+  textMessageReply: function(message) {
+    return {
+      replies: [
+        {
+          type: "text",
+          content: message
+        }
+      ]
+    };
+  },
+
+  quickReplies: function(title, buttons) {
+    return {
+      replies: [
+        {
+          type: "quickReplies",
+          content: {
+            title: title,
+            buttons: [buttons]
+          }
+        }
+      ]
+    };
+  },
+
+  pictureReply: function(imageUrl) {
+    return {
+      replies: [
+        {
+          type: "picture",
+          content: imageUrl
+        }
+      ]
+    };
+  },
+
+  videoReply: function(videoUrl) {
+    return {
+      replies: [
+        {
+          type: "video",
+          content: videoUrl
+        }
+      ]
+    };
+  },
+
+  buttonsReply: function(title, buttons) {
+    return {
+      replies: [
+        {
+          type: "buttons",
+          content: {
+            title: title,
+            buttons: [buttons]
+          }
+        }
+      ]
+    };
+  },
+
+  /* Button types:
+   - postback
+   - web_url
+   - phone_number
+  */
+  buttonObj: function(title, type, value) {
+    return {
+      title: title,
+      type: type,
+      value: value
+    };
+  },
+
+  carouselReply: function(title, subtitle, imageUrl, buttons) {
+    return {
+      replies: [
+        {
+          type: "carousel",
+          content: [
+            {
+              title: title,
+              subtitle: subtitle,
+              imageUrl: imageUrl,
+              buttons: [buttons]
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
